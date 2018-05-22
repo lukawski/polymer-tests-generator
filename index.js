660e26b44a071e8d574abbaec92c3b9f9e6f1142
Kacper Łukawski <ka.lukawski@gmail.com>
2018-05-22 21:33:08 +0200

    Injecting component import path into test.

--- index.js
+++ index.js
@@ -24,8 +24,11 @@ const getTestSeed = () => fs
   .readFile(path.join(__dirname, 'test-suite-seed.html'), 'utf-8')
   .catch(() => console.log('Can\'t get test seed.'.red));
 
-const createTest = (componentName, testSeed) => {
-  const processedSeed = testSeed.replace(/{{name}}/g, path.basename(componentName, '.html'));
+const createTest = (componentName, testSeed, compPath) => {
+  const processedSeed = testSeed
+    .replace(/{{name}}/g, path.basename(componentName, '.html'))
+    .replace(/{{importPath}}/g, path.relative('test', compPath));
+  
   return fs.writeFile(path.join(COMPONENT_PATH, 'test', componentName), processedSeed, 'utf-8')
     .then(() => console.log(`Created test for ${componentName}`.green))
     .catch(err => console.log(`Can't create test for ${componentName}. ${err}`.red));
@@ -41,14 +44,18 @@ const flatten = (files) => {
   return files.map(file => path.basename(file));
 };
 
+const findComponentPath = (compName, paths = []) => {
+  return paths.find(compPath => compPath.includes(compName));
+};
+
 const generateTests = ([files = [], existingTests = []] = []) => {
   const components = filterExisting(flatten(files), flatten(existingTests));
+
   return createTestDirectory(COMPONENT_PATH)
     .then(() => getTestSeed())
     .then(seed => {
-      return Promise.all(components.map(component => createTest(component, seed)));
+      return Promise.all(components.map(component => createTest(component, seed, findComponentPath(component, files))));
     });
-  console.log(components);
 };
 
 if (program.path) {
@@ -64,3 +71,4 @@ if (program.path) {
 } else {
   console.log('You need to provide component path!'.red);
 }
+ 
