b7bbe3857248c6b2ae0cde907cde12de4ff4eef7
Kacper Łukawski <ka.lukawski@gmail.com>
2018-05-22 20:38:32 +0200

    General refactor, using globby for files matching.

--- index.js
+++ index.js
@@ -1,110 +1,66 @@
-#!/usr/bin/env node
-const fs = require('fs-extra');
+const globby = require('globby');
 const path = require('path');
 const program = require('commander');
 const colors = require('colors');
+const fs = require('fs-extra');
 
 program
   .version('1.0.0')
   .option('-p, --path [path]')
-  .option('-n, --name [name]')
   .parse(process.argv);
 
-function isDirectory(pathToCheck) {
-  return fs.lstat(pathToCheck)
-    .then(stat => stat.isDirectory())
-    .then(isDir => (isDir ? Promise.resolve(isDir) : Promise.reject(Error('Provided path is not a directory'))));
-}
+const COMPONENT_PATH = path.resolve('', program.path);
 
-function getTestSeed() {
-  return fs.readFile(path.join(__dirname, 'test-suite-seed.html'), 'utf-8')
-    .catch(() => console.log('Can\'t get test seed.'.red));
-}
+const OPTIONS = {
+  gitignore: true,
+  cwd: COMPONENT_PATH
+};
 
-function createTestIndex(componentPath) {
-  const indexPath = path.join(componentPath, 'test', 'index.html');
-  let seedData;
-  return fs.readFile(path.join(__dirname, 'index-test-seed.html'), 'utf-8')
-    .then((data) => {
-      seedData = data;
-      return fs.readFile(indexPath, 'utf-8');
-    })
-    .then(() => { console.log('Index file already exists'.green); })
-    .catch(() => fs.writeFile(indexPath, seedData))
-    .then(() => console.log('Created index file'.green))
-    .catch(() => console.log('Can\'t create index file'.red));
-}
+const updateSuitsList = () => {
 
-function createTestDirectory(componentPath) {
-  return fs.ensureDir(path.join(componentPath, 'test'))
-    .then(() => createTestIndex(componentPath));
-}
+};
 
+const getTestSeed = () => fs
+  .readFile(path.join(__dirname, 'test-suite-seed.html'), 'utf-8')
+  .catch(() => console.log('Can\'t get test seed.'.red));
 
-function filterComponents(existing, components) {
-  const IGNORED = ['demo', 'index.html', 'test', ...existing];
-
-  return components.filter(c => !IGNORED.includes(c));
-}
-
-// function updateSuitesList(componentName) {
-//   return fs.readFile(path.join(__dirname, 'index-test-seed.html'), 'utf-8')
-//     .then((data) => {
-//       const [match] = data.match(/(\(\[)([^]*)(\]\))/);
-//       const list = match.slice(2, match.length - 2);
-//       console.log('list', list);
-//       return true;
-//     });
-// }
-
-function createTest(componentName) {
-  return getTestSeed()
-    .then(data => data.replace(/{{name}}/g, path.basename(componentName, '.html')))
-    .then(processedData => fs.writeFile(path.join(COMPONENT_PATH, 'test', componentName), processedData, 'utf-8'))
-    // .then(() => updateSuitesList(componentName))
+const createTest = (componentName, testSeed) => {
+  const processedSeed = testSeed.replace(/{{name}}/g, path.basename(componentName, '.html'));
+  return fs.writeFile(path.join(COMPONENT_PATH, 'test', componentName), processedSeed, 'utf-8')
     .then(() => console.log(`Created test for ${componentName}`.green))
     .catch(err => console.log(`Can't create test for ${componentName}. ${err}`.red));
 }
 
-function mapFiles(existingTests, components, currentDirectory = '') {
-  const filtered = filterComponents(existingTests, components);
+const createTestDirectory = componentPath => fs.ensureDir(path.join(componentPath, 'test'));
 
-  for (let i = 0, len = filtered.length; i < len; i++) {
-    const currentComponent = filtered[i];
-    const currentPath = path.resolve(COMPONENT_PATH, currentDirectory, currentComponent);
+const filterExisting = (files, existingTests) => {
+  return files.filter(file => !existingTests.includes(file))
+};
 
-    isDirectory(currentPath)
-      .then(() => fs.readdir(currentPath))
-      .then((files) => {
-        mapFiles(existingTests, files, path.join(currentDirectory, currentComponent)); 
-      })
-      .catch(() => {
-        if (path.extname(currentComponent) === '.html') {
-          createTest(currentComponent);
-        }
-      });
-  }
-}
+const flatten = (files) => {
+  return files.map(file => path.basename(file));
+};
 
-function mapFolder(err, files) {
-  if (err) return console.log('Can\'t read given path.'.red);
-
-  if (!files.length) return console.log('Given directory is empty.'.yellow);
-
-  return createTestDirectory(program.path)
-    .then(() => fs.readdir(path.join(COMPONENT_PATH, 'test')))
-    .then((existingFiles) => { mapFiles(existingFiles, files); })
-    .catch(createErr => console.log(`Something went wrong: ${createErr}`.red));
-}
-
-const COMPONENT_PATH = path.resolve('', program.path);
+const generateTests = ([files = [], existingTests = []] = []) => {
+  const components = filterExisting(flatten(files), flatten(existingTests));
+  return createTestDirectory(COMPONENT_PATH)
+    .then(() => getTestSeed())
+    .then(seed => {
+      return Promise.all(components.map(component => createTest(component, seed)));
+    });
+  console.log(components);
+};
 
 if (program.path) {
-  isDirectory(COMPONENT_PATH)
-    .then(() => {
-      fs.readdir(COMPONENT_PATH, mapFolder);
-    })
-    .catch(err => console.log(`Provided path is wrong. ${err}`.red));
+  const globbyPromises = [
+    globby(['**/*.html', '!demo', 'index.html', '!test'], OPTIONS),
+    globby('test/*.html', OPTIONS)
+  ];
+
+  Promise.all(globbyPromises)
+    .then(generateTests)
+    .then(() => console.log('🎉 🎉 🎉   Done. 🔥 🔥 🔥'.bold.underline.green))
+    .catch(err => console.log(`Something went wrong. ${err}`.red));
 } else {
   console.log('You need to provide component path!'.red);
 }
